223a126eff06c41109194943d20068ddf287aa27
John W. Murray <john@DivorceCounselor.com>
2014-03-17 19:00:28 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shop::Application.config.secret_key_base = 'b5c463e13f11b12ab6e64e694734c2042cac50ad52a9f11828af76b869d389f2bb7f907eee1b42010008d82edc54b63b5dc1715109f315e9cc196346eb2f90fc'
